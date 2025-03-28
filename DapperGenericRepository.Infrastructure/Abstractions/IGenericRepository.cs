cd47fcd0f4bbeaad99d8508b8e381e3a7ac7390c
André Conde <andreconde99@hotmail.com>
2025-03-28 00:33:05 +0000

    feat: add base generic repository

---
+++ DapperGenericRepository.Infrastructure/Abstractions/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperGenericRepository.Infrastructure.Abstractions
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetById(int id);
+        Task<IEnumerable<T>> GetAll();
+        Task<int> CountAll();
+        Task<int> Add(T entity);
+        Task<int> Update(T entity);
+        Task<int> Delete(T entity);
+    }
+
+}
---
+++ DapperGenericRepository.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperGenericRepository.Infrastructure
+{
+    public class ApplicationDbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection CreateConnection(string connectionString = "DefaultConnection")
+        {
+            string? connection = _configuration.GetConnectionString(connectionString);
+            // return new NpgsqlConnection(connection);
+            return null;
+
+        }
+    }
+
+}
